110c6a7868304eee936b7cd9fbee72ce7961e884
xrand <95123969+xrander@users.noreply.github.com>
2022-02-10 07:50:59 +0100

    Update and rename FUTA_password_generator.py to my_alma_password_generator.py

--- my_alma_password_generator.py
+++ my_alma_password_generator.py
@@ -3,5 +3,7 @@ year_employed = random.randint(1986, 2022)
 print (year_employed)
 augment = random.randint(0000, 9999)
 print (augment)
-password = str(year_employed) + str(augment)
+combo = str(year_employed) + str(augment)
+length = 8
+password = "".join(random.sample(combo, length))
 print (password)
