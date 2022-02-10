1a62f4498a8a8858a689d22580be2db89454b3d5
xrand <95123969+xrander@users.noreply.github.com>
2022-02-10 07:53:36 +0100

    Update my_alma_password_generator.py

--- my_alma_password_generator.py
+++ my_alma_password_generator.py
@@ -1,9 +1,10 @@
 import random
 year_employed = random.randint(1986, 2022)
-print (year_employed)
 augment = random.randint(0000, 9999)
-print (augment)
+
 combo = str(year_employed) + str(augment)
 length = 8
 password = "".join(random.sample(combo, length))
+
+
 print (password)
