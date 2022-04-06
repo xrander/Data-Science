33c2903694b1132e4a5621fc1a1eafef5d131141
Olamide Adu <olamideadu@gmail>
2022-04-06 11:10:02 +0200

    new script created and indexing wrapped up

---
+++ R for Datascience/data_wrangling_dplyr.R
@@ -0,0 +1,9 @@
+#data_Wrangling
+#for this, dplyr library will be introduced
+install.packages('dplyr')
+library(dplyr)
+'
+mutate() - change data table by adding a new column or changing existing column
+select() - to subset data by selecting specific columns
+filter()- to filter data by subsetting rows
+%>% - called the pipe operator, and similar to | pipe in git '
--- R for Datascience/indexing_data_wrangling_plots.R
+++ R for Datascience/indexing_data_wrangling_plots.R
@@ -2,3 +2,24 @@
 library(dslabs)
 data(murders)
 murder_rate <- murders$total/murders$population * 100000
+index <- murder_rate <= 0.71
+index
+#we can count the number of true using the code line below
+sum(index)
+#To get the states which are true
+safe <- murders$state[index]
+safe
+west <- murders$region == 'West'
+indexe <- index & west
+safe_state <- murders$state[indexe]
+safe_state
+
+#index functions
+#which, %in%, match
+#which can be used to get the index of values that are only true.
+abbs <- match(c('AK', 'MI', 'IA', 'WI'), murders$abb)
+murders$state[abbs]
+ind <- which(!abbs%in%murders$abb)
+ind
+abbs[ind]
+
