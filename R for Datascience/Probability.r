6042197eeb55999f551f5a2de237e3642309080a
Olamide Adu <olamideadu@gmail>
2022-05-02 12:47:22 +0200

    addition to probility script

--- R for Datascience/Probability.r
+++ R for Datascience/Probability.r
@@ -57,12 +57,63 @@ mean(box == 'Cyan') * (1 - mean(box == 'Cyan'))
 
 #Combination and Permutation
 #Generating decks of cards
-suits <- c('Diamonds', 'Clubs', 'Hearts', 'Spades')
+suits <- c('Diamonds', 'Clubs','Hearts', 'Spades')
 numbers <- c('Ace', 'Deuce', 'Three', 'Four', 'Five','Six','Seven',
              'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
+
 deck <- expand.grid(number=numbers, suit=suits)
+
 deck <- paste(deck$number, deck$suit)                       
 
-#Probability for kings
+#Probability for selecting a king at first draw
 kings <- paste('King', suits)
+
 mean(deck %in% kings)
+
+# PROBABILITY FOR SELECTING KING ON A SECONF DRAW WITHOUT REPLACEMENT
+
+#code for all the permutation of all the cars
+card <- permutations (52, 2, v=deck)
+
+#Grabbing the first and second column
+first_card <- card[,1]
+second_card <-card[,2]
+
+# here the first_card variable takes value from the first column while the
+# second_Card variable takes value from the second column.
+
+#to check the first_card that is a king
+sum(first_card%in%kings)
+
+#to check the probability of a selecting a king in the second card given
+# the first card selected was king is given below
+sum(first_card%in%kings & second_card%in%kings) /
+  sum(first_card%in%kings)
+
+# The code above is synonymous to 
+mean(first_card%in% kings & second_card%in%kings) /
+mean(first_card%in%kings)
+
+## Combination
+combinations(5,2)
+
+#To compute all the natural 21
+aces <- paste('Ace', suits)
+facecard <- c('King', 'Queen', 'Jack', 'Ten')
+facecard<- expand.grid(number = facecard, suit = suits)
+facecard <- paste(facecard$number, facecard$suit)
+card_hands <- combinations(52,2, v =deck)
+
+#result for the first card drawn being ace and the second a facecard
+mean(card_hands[,1] %in% aces & card_hands[,2] %in% facecard)
+
+#using a monte carlo to draw natural 21
+hand <- sample(deck,2)
+#for proportion of getting a 21
+R <- 100000
+mc <- replicate(R, {
+  hand <- sample(deck,2)
+  (hand[1]%in% aces & hand[2] %in% facecard)|
+    (hand[2] %in%aces & hand[1] %in% facecard)
+  })
+mean(mc)
